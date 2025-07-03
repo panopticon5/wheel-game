71408053360f9e6a09a3417b8033295b6c7c148d
marco <belliossimo@yahoo.it>
2025-07-03 11:32:53 +0200

    fix font issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,4 @@ import {RouterOutlet} from '@angular/router';
   templateUrl: `app.component.html`,
   styleUrl: 'app.component.scss'
 })
-export class AppComponent {
-  public title = 'spinning-wheel-game';
-}
+export class AppComponent {}
