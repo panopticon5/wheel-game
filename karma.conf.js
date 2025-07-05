ccce019b5a69b0db291af1a098329ff2e6c4179b
Marco Bellio <marco@floodflash.co>
2025-07-05 23:32:49 +0100

    use chromeheadless

--- karma.conf.js
+++ karma.conf.js
@@ -12,12 +12,7 @@ module.exports = function (config) {
       require('karma-coverage'),
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
+      jasmine: {},
     },
     jasmineHtmlReporter: {
       suppressAll: true // removes the duplicated traces
